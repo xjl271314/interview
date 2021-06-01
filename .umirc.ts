a99005082b071c8f6998e17a20302cc21ae95732
许将龙 <xujl@weipaitang.com>
2021-06-01 20:19:33 +0800

    init project

---
+++ .umirc.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from 'dumi';
+import path from 'path';
+
+const { resolve } = path;
+
+export default defineConfig({
+  title: '题库指南',
+ favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  navs: [
+    null,
+    {
+      title: 'dumi',
+      path: 'https://github.com/umijs/dumi',
+    },
+  ],
+  devServer: {
+    port: 7777,
+    host: 'localhost',
+  },
+  define: {
+    PLATFORM: 'web',
+  },
+  resolve: {
+    previewLangs: ['jsx', 'tsx'],
+  },
+  theme: {
+    '@primary-color': '#42b983',
+    '@info-background-color': '#f3f5f7',
+    '@info-border-color': '#42b983',
+    '@info-text-color': '#999',
+    '@warning-background-color': 'rgba(255,229,100,.3)',
+    '@warning-border-color': '#e7c000',
+    '@warning-text-color': '#6b5900',
+  },
+//   extraBabelPlugins: [
+//     [
+//       'import',
+//       {
+//         libraryName: 'antd',
+//         libraryDirectory: 'es',
+//         style: true,
+//       },
+//     ],
+//   ],
+  alias: {
+    app: resolve(__dirname, 'src/'),
+  },
+  links: [],
+  scripts: [
+    // {
+    //   content: require(path.join(__dirname, '/plugin.js')),
+    // },
+  ],
+});
---
+++ src/Info/index.jsx
@@ -0,0 +1,40 @@
+/**
+ * 自定义实现的类似VuePress效果的:::tip效果
+ * 目前 dumi 中插件开发的功能暂时有问题 只能使用此方式兼容
+ */
+import classNames from 'classnames';
+import React, { useMemo } from 'react';
+import styles from './index.less';
+
+/**
+ * 工具方法，可以将输入的string中用[]括起来的内容转化为html
+ * @param str 输入的string
+ * @returns html格式文本
+ */
+const formatBackQuote = (str) => {
+  return str
+    .replace(/\'(.+)'/g, `<code className="${styles.blockquote}">$1</code>`)
+    .replace(/\`(.*?)\`/g, `<em className="${styles.em}">$1</em>`);
+};
+
+/**
+ * @param {object} title optional 显示的标题
+ * @param {object} txt required 需要渲染的信息
+ */
+export default ({ type = 'info', title = '', txt }) => {
+  const showTitle = useMemo(() => {
+    if (!title && type === 'info') {
+      return '提示';
+    }
+    if (!title && type === 'warning') {
+      return '警告⚠️';
+    }
+  }, [title, type]);
+
+  return (
+    <div className={classNames(styles.components_info, styles[type])}>
+      <h2>{showTitle}</h2>
+      <pre dangerouslySetInnerHTML={{ __html: formatBackQuote(txt) }} />
+    </div>
+  );
+};
